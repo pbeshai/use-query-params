518a3ae5390b6638e192b82409a1033381fa8f0d
Peter Beshai <peter.beshai@gmail.com>
2019-09-13 18:48:00 -0400

    Add comments to no-router example

--- examples/no-router/src/App.js
+++ examples/no-router/src/App.js
@@ -5,6 +5,7 @@ import history from './history';
 function App() {
   const [, forceUpdate] = React.useReducer(x => x + 1, 0);
   React.useEffect(() => {
+    // listen for changes to the URL and force the app to re-render
     history.listen(() => {
       forceUpdate();
     });
--- examples/no-router/src/index.js
+++ examples/no-router/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { QueryParamProvider } from 'use-query-params';
 import history from './history';
-console.log('history = ', history);
 
 ReactDOM.render(
-  <QueryParamProvider>
+  /* note history={history} here is optional, but probably best for consistency */
+  <QueryParamProvider history={history}>
     <App />
   </QueryParamProvider>,
   document.getElementById('root')
