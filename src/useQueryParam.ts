b1e45de0928da4b63fc57eaafa3233102419d04a
Peter Beshai <pbeshai@netflix.com>
2020-04-16 18:04:37 -0700

    Rework code to allow dynamic changes to query param config #72

--- src/useQueryParam.ts
+++ src/useQueryParam.ts
@@ -1,18 +1,10 @@
 import * as React from 'react';
-import {
-  parse as parseQueryString,
-  parseUrl as parseQueryURL,
-  stringify,
-} from 'query-string';
-
-import {
-  EncodedQueryWithNulls,
-  StringParam,
-  QueryParamConfig,
-} from 'serialize-query-params';
+import { QueryParamConfig, StringParam } from 'serialize-query-params';
+import { getSSRSafeSearchString, usePreviousIfShallowEqual } from './helpers';
+import { sharedMemoizedQueryParser } from './memoizedQueryParser';
 import { useQueryParamContext } from './QueryParamProvider';
-import { updateUrlQuery } from './updateUrlQuery';
 import { UrlUpdateType } from './types';
+import { updateUrlQuery } from './updateUrlQuery';
 
 /**
  * Given a query param name and query parameter configuration ({ encode, decode })
@@ -30,68 +22,9 @@ import { UrlUpdateType } from './types';
  */
 export const useQueryParam = <D, D2 = D>(
   name: string,
-  paramConfig: QueryParamConfig<D, D2> = StringParam as QueryParamConfig<any>,
-  rawQuery?: EncodedQueryWithNulls
+  paramConfig: QueryParamConfig<D, D2> = StringParam as QueryParamConfig<any>
 ): [D2 | undefined, (newValue: D, updateType?: UrlUpdateType) => void] => {
   const { history, location } = useQueryParamContext();
-
-  // ref with current version history object (see #46)
-  const refHistory = React.useRef(history);
-  React.useEffect(() => {
-    refHistory.current = history;
-  }, [history]);
-
-  // ref with current version location object (see #46)
-  const refLocation = React.useRef(location);
-  React.useEffect(() => {
-    refLocation.current = location;
-  }, [location]);
-
-  // read in the raw query
-  if (!rawQuery) {
-    const locationIsObject = typeof location === 'object';
-    const windowIsDefined = typeof window !== 'undefined';
-    rawQuery = React.useMemo(() => {
-      let pathname = {};
-
-      // handle checking SSR (#13)
-      if (locationIsObject) {
-        // in browser
-        if (windowIsDefined) {
-          pathname = parseQueryString(location.search);
-        } else {
-          // not in browser
-          let url = location.pathname;
-          if (location.search) {
-            url += location.search;
-          }
-
-          pathname = parseQueryURL(url).query;
-        }
-      }
-
-      return pathname || {};
-    }, [location.search, location.pathname, locationIsObject, windowIsDefined]);
-  }
-
-  // read in the encoded string value
-  const encodedValue = rawQuery[name];
-
-  // note that we use the stringified encoded value since the encoded
-  // value may be an array that is recreated if a different query param
-  // changes. It is sufficient to use this instead of encodedValue in
-  // the useMemo dependency array since it will change any time the actual
-  // meaningful value of encodedValue changes.
-  const arraySafeEncodedValue =
-    encodedValue instanceof Array
-      ? stringify({ [name]: encodedValue })
-      : encodedValue;
-
-  // decode if the encoded value has changed, otherwise
-  // re-use memoized value
-  const decodedValue = React.useMemo(() => {
-    return paramConfig.decode(encodedValue);
-  }, [arraySafeEncodedValue, paramConfig]); // eslint-disable-line react-hooks/exhaustive-deps
 
   // create the setter, memoizing via useCallback
   const setValue = React.useCallback(
@@ -107,6 +40,34 @@ export const useQueryParam = <D, D2 = D>(
     },
     [paramConfig, name]
   );
+
+  // ref with current version history object (see #46)
+  const refHistory = React.useRef(history);
+  React.useEffect(() => {
+    refHistory.current = history;
+  }, [history]);
+
+  // ref with current version location object (see #46)
+  const refLocation = React.useRef(location);
+  React.useEffect(() => {
+    refLocation.current = location;
+  }, [location]);
+
+  // read in the raw query
+  const parsedQuery = sharedMemoizedQueryParser(
+    getSSRSafeSearchString(location)
+  );
+
+  // read in the encoded string value (we have to use previous if available because
+  // sometimes the query string changes so we get a new parsedQuery but this value
+  // didn't change, so we should avoid generating a new array or whatever value)
+  const encodedValue = usePreviousIfShallowEqual(parsedQuery[name]);
+
+  // decode if the encoded value has changed, otherwise
+  // re-use memoized value
+  const decodedValue = React.useMemo(() => {
+    return paramConfig.decode(encodedValue);
+  }, [encodedValue, paramConfig]);
 
   return [decodedValue, setValue];
 };
