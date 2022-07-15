9c27239ece10ea0220da733d6e9929db2ac5716e
Peter Beshai <pbeshai@netflix.com>
2022-07-15 12:45:09 -0700

    fix: use ts-toolbelt Narrow to not require as const on enum defaults

--- packages/serialize-query-params/src/withDefault.ts
+++ packages/serialize-query-params/src/withDefault.ts
@@ -1,5 +1,25 @@
 import { QueryParamConfig } from './types';
 
+// take from ts-toolbelt
+// https://github.com/millsp/ts-toolbelt/blob/master/sources/Function/Narrow.ts
+type Try<A1 extends any, A2 extends any, Catch = never> = A1 extends A2
+  ? A1
+  : Catch;
+
+type Narrowable = string | number | bigint | boolean;
+
+/**
+ * @hidden
+ */
+type NarrowRaw<A> =
+  | (A extends [] ? [] : never)
+  | (A extends Narrowable ? A : never)
+  | {
+      [K in keyof A]: A[K] extends Function ? A[K] : NarrowRaw<A[K]>;
+    };
+
+type Narrow<A extends any> = Try<A, [], NarrowRaw<A>>;
+
 /**
  * Wrap a given parameter with a default value when undefined or null (optionally, default includes null)
  * @param param QueryParamConfig - { encode, decode} to serialize a parameter
@@ -8,12 +28,12 @@ import { QueryParamConfig } from './types';
  */
 export function withDefault<D, DefaultType extends D2, D2 = D>(
   param: QueryParamConfig<D, D2>,
-  defaultValue: DefaultType,
+  defaultValue: Narrow<DefaultType>,
   includeNull?: false | undefined
 ): QueryParamConfig<D, Exclude<D2, undefined> | DefaultType>;
 export function withDefault<D, DefaultType extends D2, D2 = D>(
   param: QueryParamConfig<D, D2>,
-  defaultValue: DefaultType,
+  defaultValue: Narrow<DefaultType>,
   includeNull?: true
 ): QueryParamConfig<D, Exclude<D2, null | undefined> | DefaultType>;
 export function withDefault<D, DefaultType extends D2, D2 = D>(
